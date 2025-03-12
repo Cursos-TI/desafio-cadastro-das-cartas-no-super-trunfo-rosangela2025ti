1cf4f9dcdd979f26950974e413651f698717392d
rosangela2025ti <amaralrosangela180@gmail.com>
2025-03-12 20:30:22 -0300

    Update CadastrandoCartas.c

--- CadastrandoCartas.c
+++ CadastrandoCartas.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
 
+int main() {
+   
+    char estado1,codigo_da_carta1;
+    char nome_da_cidade1 [10];
+    int populaçao1,pontos_turisticos1;
+    float areakm21,pib1;
+
+    char estado2,codigo_da_carta2;
+    char nome_da_cidade2 [10];
+    int populaçao2,pontos_turisticos2;
+    float areakm22,pib2;
+
+    printf("seja bem vindo ao super trunfo em .c \n");
+
+
+    printf("digite o codigo da carta 1 :\n");
+    scanf("%c",codigo_da_carta1);
+
+    printf("digite o estado 1:\n");
+    scanf("%c",estado1);
+
+    printf("digite o nome da cidade 1:\n");
+    scanf("%s",nome_da_cidade1);
+
+
+    printf("digite_ a _populaçao 1:\n");
+    scanf("%d",populaçao1);
+
+
+    printf("digite os pontos_turisticos_da_cidade1:\n");
+    scanf("%d",pontos_turisticos1);
+
+
+    printf("digite a area_em_km2_da_cidade1:\n ");
+    scanf("%f",areakm21);
+
+
+    printf("digite o pib_da_cidade1:\n ");
+    scanf("%f",pib1);
+
+
+    
+    return 0;
+
+
+}

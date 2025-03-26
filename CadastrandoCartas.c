72c855127cd12f636c98a092f8c020e07370e289
rosangela2025ti <amaralrosangela180@gmail.com>
2025-03-26 21:37:57 +0000

    CARTA1.C

--- CadastrandoCartas.c
+++ CadastrandoCartas.c
@@ -4,45 +4,47 @@ int main() {
    
     char estado1,codigo_da_carta1;
     char nome_da_cidade1 [10];
-    int populaçao1,pontos_turisticos1;
-    float areakm21,pib1;
-
-    char estado2,codigo_da_carta2;
-    char nome_da_cidade2 [10];
-    int populaçao2,pontos_turisticos2;
-    float areakm22,pib2;
+     int populaçao1,pontos_turisticos1;
+     float areakm1,pib1;
 
     printf("seja bem vindo ao super trunfo em .c \n");
 
 
     printf("digite o codigo da carta 1 :\n");
-    scanf("%c",codigo_da_carta1);
+    scanf("%s",&codigo_da_carta1);
 
     printf("digite o estado 1:\n");
-    scanf("%c",estado1);
+    scanf("%s",&estado1);
 
     printf("digite o nome da cidade 1:\n");
-    scanf("%s",nome_da_cidade1);
+    scanf("%s",&nome_da_cidade1);
 
 
-    printf("digite_ a _populaçao 1:\n");
-    scanf("%d",populaçao1);
+    printf("digite a populaçao 1:\n");
+    scanf("%d",&populaçao1);
 
 
-    printf("digite os pontos_turisticos_da_cidade1:\n");
-    scanf("%d",pontos_turisticos1);
+    printf("digite os pontos turisticos da cidade 1:\n");
+    scanf("%d",&pontos_turisticos1);
 
 
-    printf("digite a area_em_km2_da_cidade1:\n ");
-    scanf("%f",areakm21);
+    printf("digite a area em km2 da cidade 1:\n"); 
+    scanf("%d",&areakm1);
 
 
-    printf("digite o pib_da_cidade1:\n ");
-    scanf("%f",pib1);
+    printf("digite o pib_da_cidade1:\n");
+    scanf("%f",&pib1);
 
-
-    
     return 0;
+
+    estado1=sp
+    codigo_da_carta1=a01
+    nome_da_cidade1=campinas
+    populaçao1=235.955
+    pontos_turisticos1=5
+    areakm1=27768
+    pib1=599.28
 
 
 }
+
